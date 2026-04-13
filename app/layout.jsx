363436814c299b8f743839ed7733845367428feb
Tarun <tarunappari23@gmail.com>
2026-04-13 16:37:41 +0530

    added analytics script in layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,7 @@ import TopSlider from "@/components/common/TopSlider";
 import SmoothScroll from "@/components/animations/SmoothScroll";
 import MobileNav from "@/components/common/MobileNav";
 import Cart from "@/components/cart/Cart";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-7QV49GVX69"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7QV49GVX69');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
